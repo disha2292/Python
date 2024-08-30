fff23b0ad57f330369a551eed6bd662f8f2b1391
disha2292 <disha2292@icloud.com>
2024-08-30 17:29:39 +0530

    up to date

--- CS50/Lecture-0/variables.py
+++ CS50/Lecture-0/variables.py
@@ -2,6 +2,9 @@
 
 name= input("what's your name ?")
 # print("hello , " + name )
-print("hello , ", end="")
-print(name)
+# print("hello , ", end="")
+# print(name)
 # print("hello , " , name)
+# name = name.title()
+
+print(f"hello, {name.strip().title()}")
